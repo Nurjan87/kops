f1743200ae58262da71d05fad32bb0734304d611
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-17 13:03:25 -0500

    added variable.tf

--- launchc_conf.tf
+++ launchc_conf.tf
@@ -23,7 +23,7 @@ resource "aws_launch_configuration" "bastions-kubernetesnurjan-com" {
 resource "aws_launch_configuration" "master-us-west-2a-masters-kubernetesnurjan-com" {
   name_prefix                 = "master-us-west-2a.masters.kubernetesnurjan.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.master_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesnurjan-com-cdc01ab78205ffec9c909b8a23a7b2d4.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.masters-kubernetesnurjan-com.id}"
   security_groups             = ["${aws_security_group.masters-kubernetesnurjan-com.id}"]
@@ -46,7 +46,7 @@ resource "aws_launch_configuration" "master-us-west-2a-masters-kubernetesnurjan-
 resource "aws_launch_configuration" "master-us-west-2b-masters-kubernetesnurjan-com" {
   name_prefix                 = "master-us-west-2b.masters.kubernetesnurjan.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.master_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesnurjan-com-cdc01ab78205ffec9c909b8a23a7b2d4.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.masters-kubernetesnurjan-com.id}"
   security_groups             = ["${aws_security_group.masters-kubernetesnurjan-com.id}"]
@@ -69,7 +69,7 @@ resource "aws_launch_configuration" "master-us-west-2b-masters-kubernetesnurjan-
 resource "aws_launch_configuration" "master-us-west-2c-masters-kubernetesnurjan-com" {
   name_prefix                 = "master-us-west-2c.masters.kubernetesnurjan.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.master_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesnurjan-com-cdc01ab78205ffec9c909b8a23a7b2d4.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.masters-kubernetesnurjan-com.id}"
   security_groups             = ["${aws_security_group.masters-kubernetesnurjan-com.id}"]
@@ -92,7 +92,7 @@ resource "aws_launch_configuration" "master-us-west-2c-masters-kubernetesnurjan-
 resource "aws_launch_configuration" "nodes-kubernetesnurjan-com" {
   name_prefix                 = "nodes.kubernetesnurjan.com-"
   image_id                    = "ami-0d8618ba6320df983"
-  instance_type               = "m4.large"
+  instance_type               = "${var.node_instance_type}"
   key_name                    = "${aws_key_pair.kubernetes-kubernetesnurjan-com-cdc01ab78205ffec9c909b8a23a7b2d4.id}"
   iam_instance_profile        = "${aws_iam_instance_profile.nodes-kubernetesnurjan-com.id}"
   security_groups             = ["${aws_security_group.nodes-kubernetesnurjan-com.id}"]
---
+++ variable.tf
@@ -0,0 +1,2 @@
+variable "master_instance_type" {}
+variable ""
